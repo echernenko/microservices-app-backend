c2ffa295ac9daf2c80f15c473d39dbfa95a21e3c
Eugene Chernenko <chernenko@gmail.com>
2020-11-26 10:18:50 +0100

    adding option to call external url

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from aiohttp import web
+import json, urllib.request
 
 async def handle(request):
+
+  # data = urllib.request.urlopen("https://api.github.com/users?since=100").read()
+  # res = json.loads(data)
   res = {"name1": "Leoni", "name2": "Seth", "name3": "Ulrika", "name4": "Thomas", "name5": "Benghin"}
-  return web.json_response(res)
+  return web.json_response(res);
 
 app = web.Application()
 app.router.add_get('/', handle)
